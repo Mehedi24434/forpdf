0aef07bed44352f35ae44dd9ac6c2c220e8df148
Mehedi24434 <mehedi24434@gmail.com>
2020-04-24 17:17:40 +0600

    hiiiiiii pdf

---
+++ 2nd.R
@@ -0,0 +1,36 @@
+---
+title: "2ndpdf"
+author: "Mehedi"
+date: "4/24/2020"
+output: pdf_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+# hiiiii  
+
+ **1st pdf**  
+
+ *2nd 1*  
+ - how is me [facebook](www.facebook.com)  
+ - trying  
+ - making  
+ - fooling
+```{r}
+print("hahahaha")
+```
+
+```{r cars}
+summary(cars)
+```
+
+## Including Plots
+
+You can also embed plots, for example:
+
+```{r pressure, echo=FALSE}
+plot(pressure)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
